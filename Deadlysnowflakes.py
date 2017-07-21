5633c5d5b6936e169cdaa50dc58a5a0ea7d40394
Mavis Henry <davidhenry@jeago.com>
2017-07-20 20:40:18 -0700

    New setup
    Signed-off-by: Mavis Henry <davidhenry@jeago.com>

--- Deadlysnowflakes.py
+++ Deadlysnowflakes.py
@@ -1,21 +1,27 @@
+
+possible_moves = {'right', 'left'}
+move_q = "Do you want to move {} or {}? ".format(*possible_moves)
+
 print('Death to snowflakes')
 print('a snowflake approches at an extremely high velocity')
-mymove = input()
-if mymove in ('right' , 'left'):
+mymove = input(move_q)
+if mymove in possible_moves:
     print('you narrowly avoided the deadly snowflake')
     print('a pile of snowflakes gingerly falls towards you')
-    mymove1 = input()
-    if mymove1 in ('right' , 'left'):
+    mymove1 = input(move_q)
+    if mymove1 in possible_moves:
         print('the snowflake pile cries as you dodge its embrace')
     elif mymove1 == ('hug'):
         print('the pile of snowflakes smothers you in its gentle cold embrace')
         print('how long do you embrace?')
         mytime = input()
-        if mytime in (<30):
+        if int(mytime) < 30:
             print('you feel cold but otherwise your alright')
             print('you have succesfully befriended the snowflake pile')
-        elif mytime in (>30):
+        elif int(mytime) > 30:
             print('you freeze to death thus ending you adventure')
+        else:
+            print('woo hoo')
 elif mymove == ('sword'):
     print('snowflake is cut in half')
     print('a pile of snowflakes attempts to run away from you')
