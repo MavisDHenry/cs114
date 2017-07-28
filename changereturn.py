42bd2a82fa04b2b2562d985d1cf09baefc101ffd
Mavis Henry <davidhenry@jeago.com>
2017-07-27 22:24:28 -0700

    function

--- changereturn.py
+++ changereturn.py
@@ -1,4 +1,14 @@
 print('how much change would you like?')
 change = int(input())
 coins_in_quarters = change // 25
- 
+change_left_quarters = (coins_in_quarters * 25) - change
+coins_in_dimes = change_left_quarters // 10
+change_left_dimes = (coins_in_dimes * 10) - change_left_quarters
+coins_in_nickles = change_left_dimes // 5
+change_left_nickles = (coins_in_nickles * 5) - change_left_dimes
+coins_in_pennies = change_left_nickles // 1
+change_left_pennies = (coins_in_pennies * 1) - change_left_nickles
+total_coins = ('quarters ' + str(coins_in_quarters) + " dimes " +
+               str(coins_in_dimes) + ' nickles ' + str(coins_in_nickles) +
+               ' pennies ' + str(coins_in_pennies) + '.')
+print(total_coins)
