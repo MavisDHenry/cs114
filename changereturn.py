366038f52496795a0c274e102b91b3bce5c64687
Mavis Henry <davidhenry@jeago.com>
2017-08-03 12:31:42 -0700

    Changes

--- changereturn.py
+++ changereturn.py
@@ -1,13 +1,13 @@
 print('how much change would you like?')
 change = int(input())
 coins_in_quarters = change // 25
-change_left_quarters = (coins_in_quarters * 25) - change
+change_left_quarters = change - (coins_in_quarters * 25)
 coins_in_dimes = change_left_quarters // 10
-change_left_dimes = (coins_in_dimes * 10) - change_left_quarters
+change_left_dimes = change_left_quarters - (coins_in_dimes * 10)
 coins_in_nickles = change_left_dimes // 5
-change_left_nickles = (coins_in_nickles * 5) - change_left_dimes
+change_left_nickles = change_left_dimes - (coins_in_nickles * 5)
 coins_in_pennies = change_left_nickles // 1
-change_left_pennies = (coins_in_pennies * 1) - change_left_nickles
+change_left_pennies = change_left_nickles - (coins_in_pennies * 1)
 total_coins = ('quarters ' + str(coins_in_quarters) + " dimes " +
                str(coins_in_dimes) + ' nickles ' + str(coins_in_nickles) +
                ' pennies ' + str(coins_in_pennies) + '.')
---
+++ helloworld.py
@@ -0,0 +1,26 @@
+playerhp = int(100)
+flamehp = int(200)
+playeratt = int((flamehp) - 25)
+flameatt = int(5 - playerhp)
+print('hello your about to enter a world of burning or a building that\'s on '
+      'fire')
+print('your sword has turned into a torrent of water just in the shape of a'
+      ' sword')
+print('you start in a room with a door will you go through?')
+myanswer = input()
+if myanswer == 'yes':
+    print('you walk through the door into a hallway that is covered in flame')
+    print('will you approach the flame?')
+    if myanswer == 'yes':
+        print('The flame attacks')
+        while int(flamehp) > 0:
+            print('you can fight the flame, or run from the flame what'
+                  ' will you do?')
+            myact = input()
+            if myact == "fight":
+                playeratt
+                flamehp = int(playeratt)
+                print(flamehp)
+            elif myact == "run":
+                print('you flee from the flame only to be cornered.')
+                print('will you fight or be engulfed by the flame.')
